3d0bf9bfc8f56ace52fe6b34698d9b2cfff60d67
Tadhani Urvish <tadhaniurvish9@gmail.com>
2026-02-28 14:36:03 +0530

    Updated code with new changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
 // MongoDB connection using Mongoose (CommonJS)
 const mongoose = require('mongoose');
-const url = process.env.MONGO_URI || 'mongodb://localhost:27017/ITS';
+// const url = process.env.MONGO_URI || 'mongodb://localhost:27017/ITS';
+const url = process.env.MONGO_URI || 'mongodb+srv://ITS:Dishant%4012345@cluster0.vyareyd.mongodb.net/ITS?retryWrites=true&w=majority';
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('✅ Connected to MongoDB'))
     .catch(err => console.error('❌ MongoDB connection error:', err));
+
+    
+    
